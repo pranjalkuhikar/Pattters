83a5e9db7d42a27071b64364bc2a6e94602ef908
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-03-20 11:38:34 +0530

    Refactor pattern.js to implement a new star pattern with leading spaces

--- pattern.js
+++ pattern.js
@@ -76,6 +76,25 @@ let n = 5;
 //   console.log();
 // }
 
+// for (let i = 0; i < n; i++) {
+//   for (let j = 0; j < i; j++) {
+//     process.stdout.write(" ");
+//   }
+//   for (let k = n; k > i; k--) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+
+for (let i = 0; i < n; i++) {
+  for (let j = n; j > i; j--) {
+    process.stdout.write(" ");
+  }
+  for (let k = 0; k < i; k++) {
+    process.stdout.write("* ");
+  }
+  console.log();
+}
 for (let i = 0; i < n; i++) {
   for (let j = 0; j < i; j++) {
     process.stdout.write(" ");
