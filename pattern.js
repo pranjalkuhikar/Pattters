cba1bd9612de426a8098d229aa2881ec6e1c15f6
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-03-20 13:06:22 +0530

    Refactor pattern.js to comment out X pattern printing logic

--- pattern.js
+++ pattern.js
@@ -275,13 +275,13 @@ let n = 5;
 
 // Question: Print a X pattern of stars.
 // Output:
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < n; j++) {
-    if (i == j || i + j === n - 1) {
-      process.stdout.write("*");
-    } else {
-      process.stdout.write(" ");
-    }
-  }
-  console.log();
-}
+// for (let i = 0; i < n; i++) {
+//   for (let j = 0; j < n; j++) {
+//     if (i == j || i + j === n - 1) {
+//       process.stdout.write("*");
+//     } else {
+//       process.stdout.write(" ");
+//     }
+//   }
+//   console.log();
+// }
