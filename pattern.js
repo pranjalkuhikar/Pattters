c0b64724c58de3885406cb0190ce244f741cf217
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-03-20 10:56:02 +0530

    Refactor pattern printing logic in pattern.js to reverse the output order

--- pattern.js
+++ pattern.js
@@ -1,6 +1,14 @@
 let n = 5;
+
+// for (let i = 0; i < n; i++) {
+//   for (let j = 0; j <= i; j++) {
+//     process.stdout.write("*");
+//   }
+//   console.log();
+// }
+
 for (let i = 0; i < n; i++) {
-  for (let j = 0; j <= i; j++) {
+  for (let j = n; j > i; j--) {
     process.stdout.write("*");
   }
   console.log();
