8323840f53666cf6f0c3e622229b06ad9edfee77
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-03-20 13:14:32 +0530

    Refactor pattern.js to comment out X pattern printing logic and add questions for star patterns

--- pattern.js
+++ pattern.js
@@ -262,8 +262,20 @@ let n = 5;
 //   *   *
 //    * *
 //     *
+
 // for (let i = 1; i <= n; i++) {
-//   for (let j = 0; j <= 2 * n; j++) {
+//   for (let j = 1; j <= 2 * n; j++) {
+//     if (i == j || i + j === 2 * n) {
+//       process.stdout.write("*");
+//     } else {
+//       process.stdout.write(" ");
+//     }
+//   }
+//   console.log();
+// }
+
+// for (let i = 1; i <= n; i += 2) {
+//   for (let j = 1; j <= 2 * n; j++) {
 //     if (i == j || i + j === 2 * n) {
 //       process.stdout.write("*");
 //     } else {
@@ -275,6 +287,23 @@ let n = 5;
 
 // Question: Print a X pattern of stars.
 // Output:
+// *   *
+//  * *
+//   *
+//  * *
+// *   *
+
+// for (let i = 1; i <= n; i++) {
+//   for (let j = 1; j <= n; j++) {
+//     if (i == j || i + j === n + 1) {
+//       process.stdout.write("*");
+//     } else {
+//       process.stdout.write(" ");
+//     }
+//   }
+//   console.log();
+// }
+
 // for (let i = 0; i < n; i++) {
 //   for (let j = 0; j < n; j++) {
 //     if (i == j || i + j === n - 1) {
