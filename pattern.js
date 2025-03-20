d3da1ef04722083b00daf97fb9737a4437eb4ae5
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-03-20 11:49:09 +0530

    pattern

--- pattern.js
+++ pattern.js
@@ -86,21 +86,31 @@ let n = 5;
 //   console.log();
 // }
 
+// for (let i = 0; i < n; i++) {
+//   for (let j = n; j > i; j--) {
+//     process.stdout.write(" ");
+//   }
+//   for (let k = 0; k < i; k++) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+// for (let i = 0; i < n; i++) {
+//   for (let j = 0; j < i; j++) {
+//     process.stdout.write(" ");
+//   }
+//   for (let k = n; k > i; k--) {
+//     process.stdout.write("* ");
+//   }
+//   console.log();
+// }
+
 for (let i = 0; i < n; i++) {
   for (let j = n; j > i; j--) {
     process.stdout.write(" ");
   }
-  for (let k = 0; k < i; k++) {
-    process.stdout.write("* ");
-  }
-  console.log();
-}
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < i; j++) {
-    process.stdout.write(" ");
-  }
-  for (let k = n; k > i; k--) {
-    process.stdout.write("* ");
+  for (let k = 0; k <= 2 * i; k++) {
+    process.stdout.write("*");
   }
   console.log();
 }
