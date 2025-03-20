30c79da51717d32ecf2fae54513c62c45caf6ddc
pranjalkuhikar <pranjalkuhikar123@gmail.com>
2025-03-20 11:27:38 +0530

    Refactor pattern.js to clean up commented-out code and adjust star printing logic for better alignment

--- pattern.js
+++ pattern.js
@@ -53,15 +53,25 @@ let n = 5;
 //   console.log();
 // }
 
+// for (let i = 0; i < n; i++) {
+//   for (let j = 0; j <= i; j++) {
+//     process.stdout.write("*");
+//   }
+//   console.log();
+// }
+// for (let i = 0; i < n; i++) {
+//   for (let j = n - 1; j > i; j--) {
+//     process.stdout.write("*");
+//   }
+//   console.log();
+// }
+
 for (let i = 0; i < n; i++) {
-  for (let j = 0; j <= i; j++) {
-    process.stdout.write("*");
+  for (let j = n; j > i; j--) {
+    process.stdout.write(" ");
   }
-  console.log();
-}
-for (let i = 0; i < n; i++) {
-  for (let j = n - 1; j > i; j--) {
-    process.stdout.write("*");
+  for (let k = 0; k <= i; k++) {
+    process.stdout.write("* ");
   }
   console.log();
 }
